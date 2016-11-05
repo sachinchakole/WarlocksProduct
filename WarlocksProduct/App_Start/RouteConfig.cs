aed3b7cb66060380219b2a016cf47a9b51dd7d34
ajit <ajit@idpsolutions.com.au>
2016-11-05 18:35:21 +0530

    first commit

--- WarlocksProduct/App_Start/RouteConfig.cs
+++ WarlocksProduct/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WarlocksProduct
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                defaults: new { controller = "Products", action = "Index", id = UrlParameter.Optional },
                 namespaces:new []{"WarlocksProduct.Controllers"}
             );
 
