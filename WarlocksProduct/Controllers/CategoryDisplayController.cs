44c0477927c8878c601a0bd6e8498d3cbe49bbb2
ajit <ajit@idpsolutions.com.au>
2016-11-05 20:29:03 +0530

    create Categorydisplay Controller

---
+++ WarlocksProduct/Controllers/CategoryDisplayController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WarlocksProduct.Controllers
+{
+    public class CategoryDisplayController : Controller
+    {
+        // GET: CategoryDisplay
+        public ActionResult Index(int id)
+        {
+
+            return View();
+        }
+    }
+}
